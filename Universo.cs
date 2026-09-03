680f27ee1f4102c09c397ca6cb4bdf6f4c762e69
EletronicGamerNord <eletronicgamernord@protonmail.com>
2026-09-03 19:46:08 -0300

    Adicionar arquivos de projeto.

---
+++ Universo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Universo
+{
+    private const double G = 6.674184e-11;
+    // Lista de corpos com nome "G".
+
+    // Essa é a soma e a força de todos os pares de corpos, decompondoe em Fx/Fy 
+    public void CalcularForcas() { }
+
+    // Utilizar a fórmula F=ma, depois de calcular o v=v0+at e s=s0+v0t+(a/2)t², por eixo.
+    public void AtualizarPosicoes(double tempoEntreIteracoes) { }
+
+    // Tem a função de verificar a distância entre centros < soma dos raios
+    // E se colidir, deve-se recalcular as velocidades usando a conservação de Q=mv
+    public void TratarColisoes() { }
+
+    public void ExibirEstado() { }
+
+    // Esse é o laço principal chamando os métodos acima com a "quantidade de iterações" em vezes
+    public void ExecutarSimulacao(int iteracoes, double tempoEntreIteracoes) { }
+}
