4eb074efa361f977b16cf23a2fcc5c792c835e78
Maria Júlia Barros da Silva <mjuliabarros123@gmail.com>
2026-09-10 20:22:41 -0300

    Implementação do construtor Corpos

--- Corpo.cs
+++ Corpo.cs
@@ -14,5 +14,36 @@ public class Corpo
 
     // O construtor irá recebe os 7 atributos e já calcula o Raio a partir da
     // Massa/Densidade (Essa é a fórmula do volume da esfera isolando o "r")
+
+    public Corpo(string nome, double massa, double densidade, double posX, double posY, double velX, double velY)
+    {
+         // Verifica se a massa recebida é válida.
+        if (massa <= 0)
+        {
+            throw new ArgumentException("A massa deve ser maior que zero.");
+        }
+
+        // A densidade não pode ser zero ou negativa,
+        // pois é necessária para calcular o volume do corpo.
+        if (densidade <= 0)
+        {
+            throw new ArgumentException("A densidade deve ser maior que zero.");
+        }
+        
+        // Armazena os dados iniciais recebidos pelo corpo.
+        Nome = nome;
+        Massa = massa;
+        Densidade = densidade;
+        PosX = posX;
+        PosY = posY;
+        VelX = velX;
+        VelY = velY;
+
+        // Calcula o volume do corpo
+        double volume = Massa / Densidade;
+
+        // Considerando o corpo como uma esfera, calcula seu raio a partir do volume Math.PI ≈ 3,141592... e Math.Pow(valor,  / ) calcula  a raiz cúbica.     
+        Raio = Math.Pow((3 * volume) / (4 * Math.PI), 1.0 / 3.0);
+    }   
 }
 
